3da27e5303b983dcf8035940f7db50b324038cc7
Raferty <ualexeev@gmail.com>
2022-06-25 17:17:17 +0300

    Solve MeetupDescription, MeetupCover, MeetupInfo

--- 02-components/02-MeetupDescription/MeetupDescription.js
+++ 02-components/02-MeetupDescription/MeetupDescription.js
@@ -3,5 +3,15 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupDescription',
 
-  template: `<p class="meetup-description">Описание должно быть здесь</p>`,
+  props: {
+    description: {
+      type: String,
+      require: false,
+      default() {
+        return '';
+      },
+    },
+  },
+
+  template: `<p class="meetup-description">{{ description }}</p>`,
 });
